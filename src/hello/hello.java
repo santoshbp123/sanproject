6266e6396d307d2d9506743d451ec09027105e6a
unknown <santosh@gmail.com>
2018-10-27 21:21:39 +0530

    updated master

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -5,36 +5,8 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-<<<<<<< HEAD
 
-	
-		
-		
-
-=======
->>>>>>> cf2a2693747b96203b524d3055ca16ba11781e15
-		System.out.println("sc");
-		System.out.println("sd");
-		
-		System.out.println("s3");
-		System.out.println("st");
-<<<<<<< HEAD
-
-		
-		
-		System.out.println("eeee");
-		System.out.println("rrr");
-=======
-		
-		
-		System.out.println("123");
-		System.out.println("456");
-		
-		System.out.println("678");
-		System.out.println("910");
-		
-		
->>>>>>> cf2a2693747b96203b524d3055ca16ba11781e15
+	    System.out.println("India");
 
     }
 
